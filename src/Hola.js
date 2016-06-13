3411b441df6d77bf721178219633942957cb0d4f
agalli <galli_ale@hotmail.com>
2016-06-13 20:15:47 -0300

    primer commit

---
+++ src/Hola.js
@@ -0,0 +1,12 @@
+export default class Hola{
+  constructor(nombre){
+    this.nombre=nombre;
+  }
+  getNombre(){
+    return this.nombre;
+  }
+  setNombre(nombre){
+    this.nombre=nombre;
+  }
+  
+}
---
+++ src/app.js
@@ -0,0 +1,6 @@
+import Hola from './Hola.js';
+
+var hola=new Hola("pruebas");
+console.log("hola "+hola.getNombre());
+hola.setNombre("lala");
+console.log("hola "+hola.nombre);
